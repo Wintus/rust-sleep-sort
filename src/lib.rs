891b2884ce3318137b45a500c3961ad5960f1abe
Yuya <yuya.maemichi@gmail.com>
2017-07-23 21:21:18 +0900

    Extract in library

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc::channel;
+
+// TODO: generic
+pub fn sleep_sort(ns: Vec<u32>) -> Vec<u32> {
+    let (tx, rx) = channel();
+
+    let threads: Vec<_> = ns.into_iter().map(|n| {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            let d = Duration::from_millis(n as u64 * 10);
+            thread::sleep(d);
+
+            tx.send(n).unwrap();
+        })
+    }).collect();
+
+    rx.iter()
+      .take(threads.len())
+      .collect()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,14 @@
-use std::{env, thread};
-use std::time::Duration;
-use std::sync::mpsc::channel;
+extern crate sleep_sort;
+
+use sleep_sort::sleep_sort;
+use std::env;
 
 fn main() {
-    let (tx, rx) = channel();
-
     let ns: Vec<_> = env::args()
         .skip(1)
         .map(|x| x.parse().unwrap_or(0))
         .collect();
 
-    let threads: Vec<_> = ns.into_iter().map(|n| {
-        let tx = tx.clone();
-
-        thread::spawn(move || {
-            let d = Duration::from_millis(n * 10);
-            thread::sleep(d);
-
-            tx.send(n).unwrap();
-        })
-    }).collect();
-
-    let ns: Vec<_> = rx.iter().take(threads.len()).collect();
+    let ns = sleep_sort(ns);
     println!("{:?}", ns);
 }
