7230121feabdf2dc1c3e0f1fe112622579d74fbc
Yuya <yuya.maemichi@gmail.com>
2017-07-23 20:51:49 +0900

    Change to save sorted number in shared vector

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 use std::{env, thread};
 use std::time::Duration;
+use std::sync::{Arc, Mutex};
 
 fn main() {
+    let sorted = Arc::new(Mutex::new(Vec::new()));
+
     let ns: Vec<_> = env::args()
         .skip(1)
         .map(|x| x.parse().unwrap_or(0))
         .collect();
+
     let threads: Vec<_> = ns.into_iter().map(|n| {
+        let sorted = sorted.clone();
+
         thread::spawn(move || {
             let d = Duration::from_millis(n * 10);
             thread::sleep(d);
-            println!("{}", n);
+
+            let mut sorted = sorted.lock().unwrap();
+            sorted.push(n);
         })
     }).collect();
+
     for thread in threads {
         let _ = thread.join();
     }
+
+    println!("{:?}", sorted);
 }
